616cdd2988e30dd95d51d3c4994c56973f08df4e
eldarovich99 <32201422+eldarovich99@users.noreply.github.com>
2018-12-15 12:21:55 +0300

    single imageview

--- app/src/main/java/com/to_do_list/eldarovich99/todolist/AddRecordFragment.java
+++ app/src/main/java/com/to_do_list/eldarovich99/todolist/AddRecordFragment.java
@@ -1,6 +1,10 @@
 package com.to_do_list.eldarovich99.todolist;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,14 +12,25 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class AddRecordFragment extends Fragment{
+    @BindView(R.id.cancel_button) ImageButton mCancelButton;
+    @BindView(R.id.add_photo_button) ImageButton mAddPhotoButton;
+    @BindView(R.id.apply_button) ImageButton mApplyButton;
+    @BindView(R.id.imageview) ImageView mImageView;
+    static final int GALLERY_REQUEST = 1;
+    static final int PHOTO_REQUEST = 2;
     public static AddRecordFragment newInstance() {
-
         //Bundle args = new Bundle();
-
         AddRecordFragment fragment = new AddRecordFragment();
         //fragment.setArguments(args);
         return fragment;
@@ -24,7 +39,53 @@ public class AddRecordFragment extends Fragment{
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.add_record_layout, container, false);
+        ButterKnife.bind(this, view);
+        setOnClickListeners();
         return view;
     }
+    private void setOnClickListeners(){
+        mCancelButton.setOnClickListener(v->{
+        });
+        mAddPhotoButton.setOnClickListener(v->{
+            Intent galleryPickerIntent = new Intent(Intent.ACTION_PICK);
+            galleryPickerIntent.setType("image/*");
+            startActivityForResult(galleryPickerIntent, GALLERY_REQUEST);
+        });
+        mApplyButton.setOnClickListener(v->{
+        });
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode){
+            case GALLERY_REQUEST:
+                Uri imageUri = data.getData();
+                mImageView.setImageURI(imageUri);
+                break;
+            case PHOTO_REQUEST:
+                Uri photoUri = data.getData();
+                mImageView.setImageURI(photoUri);
+                break;
+        }
+    }
+
+    /*
+    private File savePhoto() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                //".jpg",         /* suffix */
+                //storageDir      /* directory */
+        //);
+
+        // Save a file: path for use with ACTION_VIEW intents
+/*
+    Intent photoPickerIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (photoPickerIntent.resolveActivity(getActivity().getPackageManager())!=null)
+    startActivityForResult(photoPickerIntent,PHOTO_REQUEST);
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }*/
 }
---
+++ app/src/main/java/com/to_do_list/eldarovich99/todolist/DbScheme.java
@@ -0,0 +1,15 @@
+package com.to_do_list.eldarovich99.todolist;
+
+public class DbScheme {
+    public static final class ToDoListTable{
+        public static final String NAME = "to_do_list";
+        public static final class Columns{
+            public static final String UUID = "uuid";
+            public static final String TITLE = "title";
+            public static final String TEXT = "text";
+            public static final String URI = "uri";
+            public static final String DATE = "date";
+            public static final String SOLVED = "solved";
+        }
+    }
+}
